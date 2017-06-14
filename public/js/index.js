903a7abf4001b1025010f447b1be58898d55ed63
Emmanuel Nwogbo <emmnlchuck@gmail.com>
2017-06-14 18:17:09 +0000

    added js in public folder

---
+++ public/js/index.js
@@ -0,0 +1,23 @@
+var socket = io();
+
+socket.on('connect', function() {
+  console.log('connected to server');
+  //document.write('hello world');
+
+  socket.emit('createEmail', {
+    to: 'mark',
+    text: 'hey this is me'
+  });
+
+  socket.emit('createMessage', {
+    message: 'hello from here'
+  });
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnected from server');
+});
+
+socket.on('newEmail', function(email) {
+  console.log('New email', email);
+});
