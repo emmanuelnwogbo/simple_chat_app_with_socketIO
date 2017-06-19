1fbcc4915e6b40da4f7b50e1f7f8c70ec325af0d
Emmanuel Nwogbo <emmnlchuck@gmail.com>
2017-06-19 10:55:22 +0000

    if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+  // Selector
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function() {
   console.log('connected to server');
 });
@@ -18,6 +34,7 @@ socket.on('newMessage', function(message) {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
   /*var formattedTime = moment(message.createdAt).format('h:mm a');
 
   console.log('newMessage', message);
@@ -37,6 +54,7 @@ socket.on('newLocationMessage', function (message) {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 /*  var li = $('<li></li>');
   var a = $('<a target="_blank">My current Location</a>');
 
